a91b2a7b61e4913e53fc4300112fe12a3dced1f6
Harsha Vardhan <harshareddy.bathala@gmail.com>
2025-11-08 16:40:23 +0530

    Update repo name to studentMentor

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/student_mentor_ai/',
+      base: '/studentMentor/',
       server: {
         port: 3000,
         host: '0.0.0.0',
